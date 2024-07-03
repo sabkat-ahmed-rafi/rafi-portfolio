1064a5cf3429132566b15d628dbcc34f021c3b23
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-07-04 02:56:30 +0600

    email functionality added

--- src/Components/Home/Email.jsx
+++ src/Components/Home/Email.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import EmailPhoto from '../../../public/Email.json';
 import Lottie from 'lottie-react';
+import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2';
+
 
 
 const Email = () => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs
+        .sendForm('service_ytl3hrc', 'template_jkwmb0z', form.current, {
+          publicKey: 'kyzVsV5LEHpltpnis',
+        })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+            setName('')
+            setEmail('')
+            setMessage('')
+            Swal.fire("Message sent successfully!");
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+    };
+  
+
     return (
         <>
-          <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 border border-b-0">
+          <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 shadow-lg shadow-sky-300 mb-10">
 	<div className="flex flex-col justify-between">
 		<div className="space-y-2 lg:pl-14">
 			<h2 className="text-4xl font-bold leading-tight lg:text-5xl text-[#001C55] ">Let's talk!</h2>
@@ -14,18 +46,18 @@ const Email = () => {
 		</div>
         <Lottie className='lg:w-[400px] ' animationData={EmailPhoto} loop={true} />
 	</div>
-	<form noValidate="" className="space-y-6">
+	<form ref={form} onSubmit={sendEmail} className="space-y-6">
 		<div>
 			<label htmlFor="name" className="text-sm font-semibold">Full name</label>
-			<input id="name" type="text" placeholder="" className="w-full p-3 rounded dark:bg-gray-100 border border-slate-300" />
+			<input onChange={(e) => setName(e.target.value)} id="name" type="text" name='user_name' placeholder="" value={name} className="w-full p-3 rounded dark:bg-gray-100 border border-slate-300" />
 		</div>
 		<div>
 			<label htmlFor="email" className="text-sm font-semibold">Email</label>
-			<input id="email" type="email" className="w-full p-3 rounded dark:bg-gray-100 border border-slate-300" />
+			<input onChange={(e) => setEmail(e.target.value)} value={email} id="email" type="email" name='user_email' className="w-full p-3 rounded dark:bg-gray-100 border border-slate-300" />
 		</div>
 		<div>
 			<label htmlFor="message" className="text-sm font-semibold">Message</label>
-			<textarea id="message" rows="3" className="w-full p-3 rounded dark:bg-gray-100 border border-slate-300"></textarea>
+			<textarea onChange={(e) => setMessage(e.target.value)} value={message} id="message" name='message' rows="3" className="w-full p-3 rounded dark:bg-gray-100 border border-slate-300"></textarea>
 		</div>
 		<button type="submit" className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-[#001C55] dark:text-gray-50 border border-slate-300">Send Message</button>
 	</form>
